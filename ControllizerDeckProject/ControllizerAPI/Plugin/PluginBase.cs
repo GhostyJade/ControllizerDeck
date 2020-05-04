f0e19bb7fb986923c90ceec2ec9c7075b682fa5d
GhostyJade <giandreas@outlook.it>
2020-05-04 14:10:40 +0200

    Added basic plugin loading

---
+++ ControllizerDeckProject/ControllizerAPI/Plugin/PluginBase.cs
@@ -0,0 +1,18 @@
+﻿namespace ControllizerAPI.Plugin
+{
+    public abstract class PluginBase
+    {
+        public string PluginName { get; private set; }
+        public string PluginVersion { get; private set; }
+
+        protected PluginBase(string name, string version)
+        {
+            PluginName = name;
+            PluginVersion = version;
+        }
+
+        public abstract void InitPlugin();
+        public abstract void ExecutePlugin();
+        public abstract void DestroyPlugin();
+    }
+}
---
+++ ControllizerDeckProject/ControllizerDeckProject/PluginSystem/PluginLoader.cs
@@ -0,0 +1,63 @@
+﻿using ControllizerAPI.Plugin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+namespace ControllizerDeckProject.PluginSystem
+{
+    public static class PluginLoader
+    {
+        private const string DefaultPluginExtension = "*.dll";
+        private static List<Thread> pluginThreadPool = new List<Thread>();
+
+        private static Dictionary<string, PluginBase> PluginInstances = new Dictionary<string, PluginBase>();
+
+        public static bool GetAllPluginsInPath(string path)
+        {
+            if (!Directory.Exists(path))
+                return false;
+            foreach (string filename in Directory.GetFiles(path, DefaultPluginExtension, SearchOption.AllDirectories))
+            {
+                Assembly pluginDll = Assembly.LoadFile(filename);
+                foreach (var assemblyType in pluginDll.GetExportedTypes())
+                {
+                    if (assemblyType.IsSubclassOf(typeof(PluginBase))){
+
+                        var plugin = Activator.CreateInstance(assemblyType) as PluginBase;
+                        PluginInstances.Add(plugin.PluginName, plugin);
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static void InitAll()
+        {
+            foreach(PluginBase p in PluginInstances.Values)
+            {
+                p.InitPlugin();
+            }
+        }
+
+        public static void ExecuteAll()
+        {
+            foreach (PluginBase p in PluginInstances.Values)
+            {
+                pluginThreadPool.Add(new Thread(p.ExecutePlugin));
+            }
+            pluginThreadPool.ForEach(e => e.Start());
+        }
+
+        public static void DestroyAll()
+        {
+            pluginThreadPool.ForEach(e => e.Join());
+            foreach (PluginBase p in PluginInstances.Values)
+            {
+                p.DestroyPlugin();
+            }
+        }
+
+    }
+}
