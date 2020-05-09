65e3b740cd085c23ef92ede075b18b4d2b26a7ef
GhostyJade <giandreas@outlook.it>
2020-05-09 10:45:33 +0200

    Cleaned up address and port creation, added license header

--- ControllizerDeckProject/ControllizerDeckProject/Program.cs
+++ ControllizerDeckProject/ControllizerDeckProject/Program.cs
@@ -1,4 +1,22 @@
-﻿using ControlizerCore.Serial;
+﻿/*
+    Controllizer Deck - A DIY customizable controller deck
+    Copyright (C) 2020  GhostyJade <ghostyjade2410@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using ControlizerCore.Serial;
 
 using ControllizerDeckProject.Net;
 
@@ -12,7 +30,7 @@ namespace ControllizerDeckProject
         {
             SerialManager manager = new SerialManager();
             manager.Start();
-            HttpServer server = new HttpServer("http://localhost:8080/", 8080);
+            HttpServer server = new HttpServer("localhost", 8080);
             Task listen = server.Listen();
             listen.GetAwaiter().GetResult();
         }
