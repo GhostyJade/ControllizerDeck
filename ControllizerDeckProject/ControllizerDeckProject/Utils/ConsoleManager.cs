ab38bad0009467c266fa26c89b830d3327fd5f17
GhostyJade <giandreas@outlook.it>
2020-06-19 22:55:23 +0200

    Added error logger

---
+++ ControllizerDeckProject/ControllizerDeckProject/Utils/ConsoleManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ControllizerDeckProject.Utils
+{
+    public static class ConsoleManager
+    {
+        public static void LogError(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
