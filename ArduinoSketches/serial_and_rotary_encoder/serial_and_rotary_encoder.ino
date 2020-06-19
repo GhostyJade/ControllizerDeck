663c82ccc103c1f9a86daaf098cc84d8f1a4a22b
GhostyJade <giandreas@outlook.it>
2020-06-19 19:57:19 +0200

    Updated rotary encoder sketch using interrupts

--- ArduinoSketches/serial_and_rotary_encoder/serial_and_rotary_encoder.ino
+++ ArduinoSketches/serial_and_rotary_encoder/serial_and_rotary_encoder.ino
@@ -1,8 +1,8 @@
 #define BTN1 12
 #define BTN2 13
 
-#define ROTARY_DT 6
-#define ROTARY_CLK 7
+#define ROTARY_CLK 2
+#define ROTARY_DT 3
 #define ROTARY_SW 11
 
 #define BAUD_RATE 9600
@@ -23,6 +23,9 @@ void setup()
   Serial.begin(BAUD_RATE);
 
   lastCLKValue = digitalRead(ROTARY_CLK);
+
+  attachInterrupt(0, updateEncoder, CHANGE);
+  attachInterrupt(1, updateEncoder, CHANGE);
 }
 
 bool btn1Pressed = false, btn2Pressed = false;
@@ -57,8 +60,6 @@ void loop()
       Serial.write("[Btn2] Released\n");
     }
   }
-
-  updateEncoder();
 }
 
 void updateEncoder()
