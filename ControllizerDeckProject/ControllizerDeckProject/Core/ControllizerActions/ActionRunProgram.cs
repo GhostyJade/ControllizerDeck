b6e9cda0cd55bc8ccedb23495922f0bf58d624d5
GhostyJade <giandreas@outlook.it>
2020-06-20 19:36:52 +0200

    Added run program action

---
+++ ControllizerDeckProject/ControllizerDeckProject/Core/ControllizerActions/ActionRunProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ControllizerDeckProject.Core.ControllizerActions
+{
+    public class ActionRunProgram : InputActionBase
+    {
+        public string AppName = "";
+        public string FullAppDirectory = "";
+
+        public ActionRunProgram(string actionName) : base(actionName)
+        { }
+
+        public override void Execute()
+        {
+            var process = Process.Start(FullAppDirectory);
+            if (process == null)
+            {
+                // throw error
+                Console.WriteLine("Failed to start {0}", AppName);
+            }
+        }
+
+        public override void Init()
+        { }
+    }
+}
