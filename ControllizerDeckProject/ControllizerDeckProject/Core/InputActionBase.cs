a68195dab9fddd05d1930ad80eaff696f99227be
GhostyJade <giandreas@outlook.it>
2020-06-20 19:35:33 +0200

    Added Input class

---
+++ ControllizerDeckProject/ControllizerDeckProject/Core/InputActionBase.cs
@@ -0,0 +1,12 @@
+﻿namespace ControllizerDeckProject.Core
+{
+    public abstract class InputActionBase
+    {
+        public string Name { get; private set; }
+
+        public InputActionBase(string name) { Name = name; }
+
+        public abstract void Init();
+        public abstract void Execute();
+    }
+}
