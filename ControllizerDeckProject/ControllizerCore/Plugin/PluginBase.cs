d793067d7dbc0554a22cc8bd70d46f6d259db2b0
GhostyJade <giandreas@outlook.it>
2020-06-22 23:10:52 +0200

    Updated project structure

--- ControllizerDeckProject/ControllizerCore/Plugin/PluginBase.cs
+++ ControllizerDeckProject/ControllizerCore/Plugin/PluginBase.cs
@@ -16,7 +16,7 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-namespace ControllizerAPI.Plugin
+namespace ControllizerCore.Plugin
 {
     public abstract class PluginBase
     {
